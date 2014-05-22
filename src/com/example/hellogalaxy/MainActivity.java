423bc35e37857d3bb388affcf904fa1ff2e873a8
student <student@PC111S14.fgakusei.local>
2014-05-22 14:33:51 +0900

    xmlのonClickからメソッドのonClickListenerに変更

--- src/com/example/hellogalaxy/MainActivity.java
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -4,11 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
-
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,20 +13,11 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	public void switchC(View v){
-		EditText tv1 = (EditText)findViewById(R.id.editText1);
-		EditText tv2 = (EditText)findViewById(R.id.editText2);
-		tv1.setText("");
-		tv2.setText("");
-	}
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
 
-	public void switchO(View v){
-		EditText tv1 = (EditText)findViewById(R.id.editText1);
-		EditText tv2 = (EditText)findViewById(R.id.editText2);
-		TextView tv3 = (TextView)findViewById(R.id.textView3);
-		tv3.setText("Welcome!" + tv1.getText().toString() + " " + tv2.getText().toString());
 	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
